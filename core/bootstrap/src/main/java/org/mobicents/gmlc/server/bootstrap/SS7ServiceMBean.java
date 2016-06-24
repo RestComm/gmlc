c4c7694a3ed2c40fcfc4e413e9ec710b122e4d6e
Andrew Eross <angrygreenfrogs@gmail.com>
2016-06-24 10:59:34 -0400

    Corrected path for location of bootstrap source to fix the "Missing version.properties" error at run-time

--- core/bootstrap/src/main/java/org/mobicents/gmlc/server/bootstrap/SS7ServiceMBean.java
+++ core/bootstrap/src/main/java/org/mobicents/gmlc/server/bootstrap/SS7ServiceMBean.java
@@ -20,37 +20,37 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.mobicents.applications.gmlc.bootstrap;
+package org.mobicents.gmlc.server.bootstrap;
 
 /**
  *
  * @author kulikov
  */
 public interface SS7ServiceMBean {
-	public static final String ONAME = "org.mobicents.ss7:service=SS7Service";
+    public static final String ONAME = "org.mobicents.ss7:service=SS7Service";
 
-	public void start() throws Exception;
-	public void stop();
+    public void start() throws Exception;
+    public void stop();
      /**
       * Returns SCCP Provider jndi name.
       */
-	 public String getJndiName();
-	 
-	 /**
-	  * Returns SS7 Name for this release
-	  * @return
-	  */
-	 public String getSS7Name();
-	 
-	 /**
-	  * Get Vendor supporting this SS7
-	  * @return
-	  */
-	 public String getSS7Vendor();
-	 
-	 /**
-	  * Return SS7 version of this release
-	  * @return
-	  */
-	 public String getSS7Version();
+     public String getJndiName();
+
+     /**
+      * Returns SS7 Name for this release
+      * @return
+      */
+     public String getSS7Name();
+
+     /**
+      * Get Vendor supporting this SS7
+      * @return
+      */
+     public String getSS7Vendor();
+
+     /**
+      * Return SS7 version of this release
+      * @return
+      */
+     public String getSS7Version();
 }
